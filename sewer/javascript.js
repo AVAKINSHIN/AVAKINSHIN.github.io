71ac60c1493a4ddcb41d5644ee31163402bc79ce
AVAKINSHIN <avakinshin485@gmail.com>
2024-10-12 21:53:41 +0300

    Update javascript.js

--- sewer/javascript.js
+++ sewer/javascript.js
@@ -7,7 +7,7 @@ function calculate(event) {
     event.preventDefault();
 
     let result = document.getElementById("result");
-    let forest = document.getElementById("forest");
+    let forest = document.getElementById("forest").value;
     let priceElement = document.getElementById("product").value;
     let countElement = document.getElementById("quantity").value;
 
