ce1e1d84eef27917ddcc5f6913e1ffe05e8563d3
AVAKINSHIN <avakinshin485@gmail.com>
2024-10-14 08:40:47 +0300

    Create javascript.js

---
+++ dark/javascript.js
@@ -0,0 +1,29 @@
+function isNumber(str) {
+    return str.match(/^[1-9]\d*$/) !== null;
+}
+
+
+function calculate(event) {
+    event.preventDefault();
+
+    let result = document.getElementById("result");
+    let priceElement = document.getElementById("product").value;
+    let countElement = document.getElementById("quantity").value;
+
+    let c;
+
+    if (priceElement === "") {
+        c = "Выберите продукцию!";
+    } else if (!isNumber(countElement)) {
+        c = "Введите целое число!";
+    } else {
+        c = parseInt(priceElement) * parseInt(countElement);
+    }
+    forest.innerHTML = parseInt(priceElement);
+    result.innerHTML = c;
+}
+
+
+window.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("btn").addEventListener("click", calculate);
+});
