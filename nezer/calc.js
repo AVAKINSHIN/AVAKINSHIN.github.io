ff32bf83ba5fcd2d0825476bb67068847cf395d4
AVAKINSHIN <avakinshin485@gmail.com>
2024-10-19 23:17:40 +0300

    Update calc.js

--- nezer/calc.js
+++ nezer/calc.js
@@ -28,7 +28,7 @@ function get_products() {
         },
         {
             name: "Наушники",
-            price: 819,
+            price: 419,
             properties: [
                 {
                     name: "Помощь в запуске гаджета",
@@ -64,23 +64,18 @@ function get_products() {
                     price: 300
                 },
                 {
-                    name: "Доставка на дом с дополнительной эксплуатацией",
+                    name: "Доставка с дополнительной эксплуатацией",
                     price: 500
-                },
             ],
-            price: 1721,
+            price: 4171,
             properties: [
                 {
-                    name: "Помощь в запуске гаджета",
-                    price: 100
+                    name: "Hearts of Stone",
+                    price: 449
                 },
                 {
-                    name: "Дополнительная симкарта",
-                    price: 300
-                },
-                {
-                    name: "Гарантия на 1 год",
-                    price: 500
+                    name: "Blood and Wine",
+                    price: 349
                 }
             ]
         }
