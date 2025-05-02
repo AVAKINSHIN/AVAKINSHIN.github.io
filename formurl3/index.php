01e5a2da2526db314f84c5251eae4097de02fd21
AVAKINSHIN <avakinshin485@gmail.com>
2025-05-02 22:38:10 +0300

    Update index.php

--- formurl3/index.php
+++ formurl3/index.php
@@ -196,7 +196,16 @@ else
           [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
   if (!empty($COOKIE[session_name()]) && session_start() && !empty($_SESSION['login']))
   {
-
+          $stmt = $db->prepare("UPDATE person SET fio=?, phone=?, email=?, year=?, gender=?, biography=?, accept=? WHERE id=?");
+        $stmt->execute([$_POST['fio'], $_POST['phone'], $_POST['email'], $_POST['year'], $_POST['gender'], 
+                       $_POST['biography'], $_POST['accept'], $_SESSION['uid']]);
+            $stmt = $db->prepare("DELETE FROM person_language WHERE id_p=?");
+            $stmt->execute([$_SESSION['uid']]);
+            foreach ($_POST['language'] as $valuclient_loop: send disconnect: Connection reset
+            {
+                $stmt = $db->prepare("INSERT INTO person_language SET id_l=?, id_p=?");
+                $stmt->execute([$v, $_SESSION['uid']]);
+            }
   }
   else
   {
