deff0bf65b676c4e471bd186de145f33f0f925d1
AVAKINSHIN <avakinshin485@gmail.com>
2025-05-08 16:50:39 +0300

    Update login.php

--- formurl3/login.php
+++ formurl3/login.php
@@ -1,54 +1,101 @@
-<?php
-header('Content-Type: text/html; charset=UTF-8');
-$session_started = false;
-if ($_COOKIE[session_name()] && session_start()) {
-  $session_started = true;
-  if (!empty($_SESSION['login'])) {
-if (isset($_COOKIE[session_name()]) && session_start()) {
-  $session_started = true;
-  if (!empty($_SESSION['login']))
-  {
-          if (isset($_POST['exit']))
+<?php                                                                                                                                                       
+header('Content-Type: text/html; charset=UTF-8');                                                                                                           
+$session_started = false;                                                                                                                                   
+if (isset($_COOKIE[session_name()]) && session_start()) {                                                                                                    
+  $session_started = true;                                                                                                                                   
+  if (!empty($_SESSION['login']))                                                                                                                           
+  {                            
+    if (isset($_POST['exit']))     
+      {                                  
+        session_destroy();                  
+        header('Location: login.php');          
+        exit();                                   
+        }                                          
+    $languages=empty($_COOKIE['language_value'])?array():explode("|", $_COOKIE['language_value']);
+          $db = new PDO('mysql:host=localhost;dbname=uXXXXX', 'uXXXXX', 'XXXXXXX',                                                                       
+                        [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+          if ($_SERVER['REQUEST_METHOD'] == 'GET')
           {
-                session_destroy();
-                header('Location: login.php');
-                exit();
-          }
-?>
-Вы уже авторизовались в системе. Хотите выйти или продолжить? <br />
-<form action="./" method="post">
-<input type="submit" value="Продолжить" />
-</form>
+                  $errors = array();
+                  $values = array();
+                foreach (array('fio', 'phone', 'email', 'year', 'gender', 'language', 'biography', 'accept') as $v)
+                {
+                        $errors[$v] = !empty($_COOKIE[$v.'_error']);
+                        if ($errors[$v])
+                        {
+                                $messages[] = sprintf('<div class="error"><strong>%s</strong></div>', strip_tags($_COOKIE[$v.'_error']));
+                                setcookie($v.'_error', '', 100000);
+                                setcookie($v.'_value', '', 100000);
+                        }
+                }
+                $stmt=$db->prepare("SELECT fio, email, year, gender, phone, biography, accept FROM person WHERE id=?");
+                $stmt->execute([$_SESSION['uid']]);
+                if ($stmt->rowCount()!=0)
+                {
+                  $row=$stmt->fetch(PDO::FETCH_ASSOC);
+                  foreach (array('fio', 'phone', 'email', 'year', 'gender', 'biography', 'accept') as $v)
+                  {
+                        $values[$v] = strip_tags($row[$v]);
+                  }
+                }
+                printf('Вход с логином %s, uid %d', $_SESSION['login'], $_SESSION['uid']);
+                include("form.php");
+?>                                                                                                                                                          
 <form action="login.php" method="post">
 <input type="submit" value="Выйти" name="exit" />
 </form>
 <?php
-          exit();
+                exit();
+          }
+          else
+          {
+                  include("errortest.php");
+                   if ($errors)
+                   {
+                        header('Location: login.php');
+                        exit();
+                   }
+                $stmt = $db->prepare("UPDATE person SET fio=?, phone=?, email=?, year=?, gender=?, biography=?, accept=? WHERE id=?");
+                $stmt->execute([$_POST['fio'], $_POST['phone'], $_POST['email'], $_POST['year'], $_POST['gender'],                                                             
+                               $_POST['biography'], $_POST['accept'], $_SESSION['uid']]);
+                $stmt = $db->prepare("DELETE FROM person_language WHERE id_p=?");                                                                                   
+            $stmt->execute([$_SESSION['uid']]);
+            foreach ($_POST['language'] as $v)
+            {                                                                                                                                                       
+              $stmt = $db->prepare("INSERT INTO person_language SET id_l=?, id_p=?");
+                $stmt->execute([$v, $_SESSION['uid']]);
+            }
+            header("Location: login.php");
+            exit();
+          }
   }
 }
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+if ($_SERVER['REQUEST_METHOD'] == 'GET')
+{
 ?>
 
 <form action="" method="post">
-  <input name="login" />
-  <input name="pass" />
+  Логин: <input name="login" />
+  Пароль: <input name="pass" />
+  UID: <input name="uid" />
   <input type="submit" value="Войти" />
 </form>
 
 <?php
 }
-else {
+else
+{
         if (empty($_POST['login']) || empty($_POST['pass']))
         {
                 print("Неверный логин или пароль<br />");
                 print("<a href='login.php'>Попробовать снова</a> <br />");
                 print("<a href='index.php'>Продолжить как гость</a>");
-                exit();
+          exit();
         }
          $db = new PDO('mysql:host=localhost;dbname=uXXXXX', 'uXXXXX', 'XXXXXXX',
                  [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-        $stmt=$db->prepare("SELECT id_user, pass FROM users WHERE name=?");
-                $stmt->execute([$_POST['login']]);
+        $stmt=$db->prepare("SELECT name, pass FROM users WHERE id_user=?");
+                $stmt->execute([$_POST['uid']]);
                 if ($stmt->rowCount() == 0)
                 {
                         print("Неверный логин или пароль<br />");
@@ -57,7 +104,7 @@ else {
                         exit();
                 }
                 $row=$stmt->fetch(PDO::FETCH_ASSOC);
-                if (!password_verify($_POST['pass'], $row['pass']))
+                if (!password_verify($_POST['pass'], $row['pass']) || $_POST['login']!=$row['name'])
                 {
                         print("Неверный логин или пароль<br />");
                         print("<a href='login.php'>Попробовать снова</a> <br />");
@@ -69,7 +116,7 @@ else {
                         session_start();
                 }
                 $_SESSION['login'] = $_POST['login'];
-                $_SESSION['uid'] = $row['id_user'];
-                header('Location: ./');
+                $_SESSION['uid'] = $_POST['uid'];
+                header("Location: login.php");
 }
 ?>
