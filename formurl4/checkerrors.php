98baeb481ebe0e5f9501bbdd7daccc1f7047a4d4
AVAKINSHIN <avakinshin485@gmail.com>
2025-05-08 20:09:36 +0300

    Update checkerrors.php

--- formurl4/checkerrors.php
+++ formurl4/checkerrors.php
@@ -1,1 +1,14 @@
-
+<?php
+$errors = array();
+$values = array();
+foreach (array('fio', 'phone', 'email', 'year', 'gender', 'language', 'biography', 'accept') as $v)                                             
+  {                                                                                                                                             
+    $errors[$v] = !empty($_COOKIE[$v.'_error']);                                                                                                 
+    if ($errors[$v])                                                                                                                           
+    {                                                                                                                                         
+      $messages[] = sprintf('<div class="error"><strong>%s</strong></div>', strip_tags($_COOKIE[$v.'_error']));                             
+      setcookie($v.'_error', '', 100000);                                                                                                    
+      setcookie($v.'_value', '', 100000);                                                                                                     
+    }                                                                                                                                         
+  }
+?>
