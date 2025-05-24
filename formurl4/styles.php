77c1de7b168d79f2d358e83c3920d6d99e19d66b
AVAKINSHIN <avakinshin485@gmail.com>
2025-05-24 16:53:39 +0300

    Create styles.php

---
+++ formurl4/styles.php
@@ -0,0 +1,28 @@
+<html>
+  <head>
+    <style>
+        input.n {
+                background-color: blue;
+                color: white;
+        }
+        textarea {
+                width: 500px;
+                height: 500px;
+        }
+        textarea.n {
+                background-color: red;
+                color: white;
+        }
+        .error {
+                border: 2px solid red;
+                color: white;
+                background-color: #781F19;
+        }
+        .save {
+                border: 2px solid blue;
+                background-color: green;
+                color: white;
+        }
+    </style>
+  </head>
+  <body>
