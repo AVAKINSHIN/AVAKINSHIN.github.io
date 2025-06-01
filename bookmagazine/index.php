c1a0dd783360d6783af3ea9b3496d1f5ca382350
AVAKINSHIN <avakinshin485@gmail.com>
2025-06-01 23:03:05 +0300

    Update index.php

--- bookmagazine/index.php
+++ bookmagazine/index.php
@@ -1,1 +1,38 @@
-
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+if (isset($_COOKIE[session_name()]) && session_start() && !empty($_SESSION['login']))
+{
+        $min = intval(date('i')) - $_SESSION['min'];
+        if ($_SESSION['date'] != date('Y-m-d') || $_SESSION['hr'] != date('H') || $min > 30 || isset($_POST['exit']))
+        {
+                session_destroy();
+                header('Location: index.php');
+                exit();
+        }
+        if (isset($_COOKIE['page']))
+        {
+                if ($_COOKIE['page'] == "bookpage")
+                {
+                        include("bookpage.php");
+                        exit();
+                }
+                if ($_COOKIE['page'] == "add")
+                {
+                        include("formaddbook.php");
+                        exit();
+                }
+                if ($_COOKIE['page'] == 'smenarol')
+                {
+                        include("smenarol.php");
+                        exit();
+                }
+                include("mybooks.php");
+                exit();
+        }
+        include("mainpage.php");
+}
+else
+{
+        include("login.php");
+}
+?>
