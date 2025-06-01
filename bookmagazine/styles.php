f0db8b36934836c1e52e12a711bb8d3bb756b23e
AVAKINSHIN <avakinshin485@gmail.com>
2025-06-01 23:32:01 +0300

    Create styles.php

---
+++ bookmagazine/styles.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+background: #D0F0C0;
+}
+#container {  display: flex; flex-direction: column; gap: 20px; align-items: safe center; }
+.b {
+background: #006400;
+color: white;
+margin: auto;
+text-align: center;
+border: 2px solid #3BB08F;
+width: 95%;
+}
+.c {
+background: #1A4876;
+color: white;
+text-align: center;
+margin: auto;
+width: 95%;
+border: 2px solid #4B0082;
+}
+input {
+width: 60%;
+background: #00FFFF;
+display: inline-block;
+border: 2px solid #310062;
+color: #000000;
+}
+input.o {
+width: 15px;
+}
+input.klen {
+background: #015D52;
+color: #FFFFFF;
+width: 60%;
+text-align: center;
+}
+img
+{
+width: 80%;
+}
+img.q
+{
+width: 95%;
+}
+td.x
+{
+color: #FFFFFF;
+}
+td.z
+{
+text-align: center;
+background: #00FFFF;
+border: 2px solid #3BB08F;
+color: #000000;
+}
+.error {
+                border: 2px solid #480607;
+                color: white;
+                background-color: #7B001C;
+                width: 90%;
+                margin: auto;
+                text-align: center;
+        }
+@media (min-width: 500px)
+{
+img {
+width: 200px;
+}
+img.q
+{
+width: 500px;
+}
+input {
+width: 500px;
+}
+input.o {
+width: 15px;
+}
+input.klen {
+width: 300px;
+}
+.error {
+width: 500px;
+}
+}
+@media (min-width: 800px)
+{
+.b {
+width: 800px;
+}
+.c {
+width: 800px;
+}
+}
+</style>
+<script defer src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script defer src="https://cdn.jsdelivr.net/npm/slick-carousel/slick/slick.min.js"></script>
+<script defer src="calc.js"></script>
